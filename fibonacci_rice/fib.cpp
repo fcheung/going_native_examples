be2d1d9bb93a3614d10af829f006699beb0d9a3f
Frederick Cheung <frederick.cheung@gmail.com>
2013-09-23 09:59:53 +0100

    toy rice example

---
+++ fibonacci_rice/fib.cpp
@@ -0,0 +1,19 @@
+#include "rice/Class.hpp"
+
+using namespace Rice;
+
+Object calculate_fibonacci(Object self, long n){
+  long result = 1, previous=0;
+  for(long i=0; i< n-1;i++){
+    long old_previous = previous;
+    previous = result;
+    result = old_previous + result;
+  }
+  return to_ruby(result);
+}
+
+extern "C"
+void Init_fib(){
+  Class rb_cTest = define_class("Fibonnaci").
+                      define_singleton_method("value", &calculate_fibonacci);
+}
