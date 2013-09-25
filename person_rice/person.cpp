5321bea1666c5de7ab60016a9c8adf027eff7689
Frederick Cheung <frederick.cheung@gmail.com>
2013-09-25 23:53:08 +0100

    small rice object example

---
+++ person_rice/person.cpp
@@ -0,0 +1,28 @@
+#include "rice/Class.hpp"
+#include "rice/String.hpp"
+#include "rice/Constructor.hpp"
+
+using namespace Rice;
+
+class Person {
+  public:
+    Person():m_name(""){}
+    const std::string& get_name() const{
+        return m_name;
+    }
+    
+    void set_name(const std::string& new_name){
+        m_name = new_name;
+    }
+  private:
+    std::string m_name;  
+};
+
+extern "C"
+void Init_person(){
+    Data_Type<Person> rb_cPerson =
+    define_class<Person>("Person").
+      define_constructor(Constructor<Person>()).
+      define_method("name", &Person::get_name).
+      define_method("name=",&Person::set_name);
+}
